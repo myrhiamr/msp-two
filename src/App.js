f5fa591a19ff3d9c89e66723f89066a173f026aa
Myrhiam Rodriguez <myrhiam74@gmail.com>
2024-05-07 21:00:09 -0700

    clean

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ function App() {
   return (
     <div>
      <h1>MILESTONE PROJECT 2 </h1>
-     <p>good luck cute pie! you got this!</p>
-     <p>hello</p>
-     <p>this is a test</p>
-     <h2>Hey hi</h2>
+     
     </div>
   );
 }
 
 
 export default App;
-``
